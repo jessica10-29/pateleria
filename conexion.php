bf979706fabfb2b59db6aef7260c492c0bb5fb75
dayana <seguraordonezpaola@gmail.com>
2026-01-03 11:15:42 -0500

    cambio2

--- conexion.php
+++ conexion.php
@@ -1,8 +1,8 @@
 <?php
-$host = "localhost:3307";
-$user = "root";
-$pass = "";
-$db = "pasteleria";
+$host = "sql206.infinityfree.com";
+$user = "if0_40818115";
+$pass = "jjsh12347";
+$db = "if0_40818115_pateleria";
 
 $conn = new mysqli($host, $user, $pass, $db);
 
